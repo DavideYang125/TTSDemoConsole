71559e4679b9716ffdfde3fd5666071d141013ae
yangww <dayang_techmail@163.com>
2020-06-29 11:47:13 +0800

    speech

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Speech.Synthesis;
 using System.Text;
@@ -14,6 +15,15 @@ namespace TTSDemoConsole
             var synthesizer = new SpeechSynthesizer();
             synthesizer.SetOutputToDefaultAudioDevice();
             synthesizer.Speak("All we need to do is to make sure we keep talking");
+            Console.WriteLine("Press any key to exit...");
+            synthesizer.SetOutputToWaveFile(@"D:\tts\en_1.wav");
+            var content = File.ReadAllText(@"D:\tts\v.txt", Encoding.UTF8);
+
+            synthesizer.SelectVoice("Microsoft Huihui Desktop");
+            synthesizer.SetOutputToWaveFile(@"D:\tts\cn_1.wav");
+            synthesizer.Speak("你好，早上好，");
+            synthesizer.SetOutputToWaveFile(@"D:\tts\cn_2.wav");
+            synthesizer.Speak(content);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
