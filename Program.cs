08c85ab9be6699a839a3ba4fa6402c09247fc3bd
yangww <dayang_techmail@163.com>
2020-06-29 10:06:07 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTSDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Speak("All we need to do is to make sure we keep talking");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
